b02b9857ef25ca34ca8993004d27583f94f1ceeb
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-14 17:34:17 +0100

    Improved the shema

--- API/models/users.js
+++ API/models/users.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 // Create a basic schima for the login
 const Schima = mongoose.Schema;
 const UsersSchima = new Schima({
-    userName: String,
-    email: String,
-    password: String,
-    date: {type: Date, default: Date.now}
+    userName: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    date: {type: Date, default: Date.now},
+    updated_date: {type: Date, default: Date.now}
 })
 
 export const Users = mongoose.model('users', UsersSchima);
