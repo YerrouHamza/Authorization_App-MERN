aa40448e98d0904f5dab8eed38da5007e3a4d645
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-14 16:23:06 +0100

    config the dotenv and start useing the port and dburl form .env file

--- API/server.js
+++ API/server.js
@@ -1,21 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import login from './routes/login.js'
+import dotenv from 'dotenv';
+
+// config the dotenv so we can use variabels form the .env file
+dotenv.config();
+const PORT = process.env.SERVER_PORT;
+const dbUrl = process.env.DB_URL;
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 // get the mongodb database and connected it with express js
-const dbUrl = 'mongodb://localhost:27017/login_study';
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbUrl)
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('MongoDB connection error:', error));
 
 // Use the route file for the Login
 app.use('/api/auth', login)
 
-const PORT = '8080'
 app.listen(PORT, () => {
     console.log(`Server runing on port ${PORT}`)
 })
