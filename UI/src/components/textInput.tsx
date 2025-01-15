e584626a348a74641289de45e2749d1b09baae89
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-15 23:17:34 +0100

    craeted the textInput component

---
+++ UI/src/components/textInput.tsx
@@ -0,0 +1,35 @@
+import cn from "../lib/utils";
+
+export default function TextInput ({
+    value,
+    label,
+    onChange,
+    type = 'text',
+    required = false,
+    placeholder = '',
+    className = '',
+    ...props
+}: {
+    value: string;
+    label: string
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+    required?: boolean;
+    placeholder?: string;
+    className?: string;
+}) {
+    return (
+        <div className="space-y-2">
+            {label && <label>{label}</label>}
+            <input
+                type={type}
+                value={value}
+                className={cn('h-11 px-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring focus:border-blue-300', className)}
+                onChange={onChange}
+                required={required}
+                placeholder={placeholder}
+                {...props}
+            />
+        </div>
+    ) 
+}
