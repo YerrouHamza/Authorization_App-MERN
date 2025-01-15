8ddfef0e10baf4e0d19bcc9b9e99a29202a1e428
yerrouhamza <hamzayerrou5@yahoo.com>
2025-01-15 16:21:53 +0100

    imported the react route dom and configed it with created the base pages

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./views/homePage"
+import LoginPage from "./views/loginPage"
+import RegisterPage from "./views/registerPage"
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-green-800">Test Tailwind</h1>
-    </div>
+    <Routes>
+      <Route path="/" Component={HomePage} />
+      <Route path="/login" Component={LoginPage} />
+      <Route path="/register" Component={RegisterPage} />
+    </Routes>
   )
 }
 
